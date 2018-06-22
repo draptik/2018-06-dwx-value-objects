ebc4eb50ac06a0a45a4066c3539790febb5d6bce
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-06-22 15:38:29 +0200

    valid email test

--- code/csharp/Demos/Examples.Tests/MailDemo/EmailTests.cs
+++ code/csharp/Demos/Examples.Tests/MailDemo/EmailTests.cs
@@ -20,5 +20,14 @@ namespace Examples.Tests.MailDemo
             Action action = () => new Email(invalidInput);
             action.Should().Throw<InvalidEmailException>();
         }
+
+        [Theory]
+        [InlineData("localhost")]
+        [InlineData("a@b.c")]
+        public void Creating_Email_should_not_throw_when_given_nvalid_input(string validInput)
+        {
+            Action action = () => new Email(validInput);
+            action.Should().Throw<InvalidEmailException>();
+        }
     }
 }
